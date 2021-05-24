8174df6f7088073d6cdfe91d2584d0d9efe6ce62
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-05-24 17:21:37 +0200

    nim game setup

---
+++ nim/nim.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+
+int main(int argc, char* argv[])
+{
+    int matches = 24;
+    int dOne = 1;
+    int dTwo = 2;
+    int dThree = 3;
+    
+    std::cout << "Welcome to the NIM-Game" << std::endl;
+
+    // function which subtracts matches. If draw > 3 draw = false, return.
+    // cin subtractmatches(1-3) something
+    // display remaining
+    // loop until matches = 0.
+    
+    while(matches <= 0)
+
+        
+    
+    
+        std::cout << "Thank you for playing." << std::endl;
+    return 0;
+}
