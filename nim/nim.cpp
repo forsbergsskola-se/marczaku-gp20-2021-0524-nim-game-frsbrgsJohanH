642099b16c3d7c9f1d845b5ca57721ebd19133bf
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-05-24 22:16:13 +0200

    single player nim game

--- nim/nim.cpp
+++ nim/nim.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 
+using namespace std;
+
+
+
 
 
 int main(int argc, char* argv[])
 {
-    int matches = 24;
-    int dOne = 1;
-    int dTwo = 2;
-    int dThree = 3;
+   int matches = 24;
+    bool gameRunning = true;
     
-    std::cout << "Welcome to the NIM-Game" << std::endl;
+    cout << "Welcome to the NIM-Game" << endl;
 
-    // function which subtracts matches. If draw > 3 draw = false, return.
-    // cin subtractmatches(1-3) something
-    // display remaining
-    // loop until matches = 0.
-    
-    while(matches <= 0)
+    while(gameRunning == true)
+    {
+        cout << "Take matches. 1, 2 or 3." << endl;
+        int input{};
+        cin >> input;
 
-        
+        if(input > 3 || input < 1)
+        {
+            cout << "You can only take 1, 2 or 3 matches." << endl;
+        }
+
+        if (matches != 0)
+        {
+             
+            cout << int (matches -= input);
+           
+            gameRunning = true;
+        }
+        else if(matches <= 0)
+        {
+            gameRunning = false;
+        }
+    }
     
-    
-        std::cout << "Thank you for playing." << std::endl;
-    return 0;
+
+        cout << "Thank you for playing." << endl;
+   return 0;
 }
+
+// function which subtracts matches. If draw > 3 draw = false, return.
+// cin subtractmatches(1-3) something
+// display remaining
+// loop until matches = 0.
+    
