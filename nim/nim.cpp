9dcf8a21a26989396da14f688c2fad3cfa2002bb
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-05-25 19:06:03 +0200

    Player 1 & 2 can play, but no one can win or lose :)

--- nim/nim.cpp
+++ nim/nim.cpp
@@ -7,54 +7,66 @@ int matches = 24;
 
 int playerOne(int remainingMatches)
 {
+    
     remainingMatches = matches;
     
-    cout << "There is " << + remainingMatches << " matches left!" << endl;
-    cout << "Player 1. Take 1, 2 or 3 Matches." << endl;
-    int input;
-    cin >> input;
-
-    if(!cin || input > 3 || input < 1)
-    {
-        cout << "You can only take 1, 2 or 3 matches." << endl;
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        
-    }
-    else
-    {
-        matches -= input;
-        return matches;
-    }
-
     
+        
+        while(true)
+        {
+            cout << "There is " << + remainingMatches << " matches left!" << endl;
+            cout << "Player 1. Take 1, 2 or 3 Matches." << endl;
+            int input;
+            cin >> input;
+
+            if(!cin || input > 3 || input < 1)
+            {
+                cout << "You can only take 1, 2 or 3 matches." << endl;
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+            }
+            else
+            {
+                matches -= input;
+                return matches;
+            }
+         
+        }
+       
+        
+    
+  
 }
 
 int playerTwo(int remainingMatches)
 {
+    
     remainingMatches = matches;
     
-    cout << "There is " << + remainingMatches << " matches left!" << endl;
-    cout << "Player 2. Take 1, 2 or 3 Matches." << endl;
-    int input;
-
-    cin >> input;
-
-    if(!cin || input > 3 || input < 1)
+    while(true)
     {
-        cout << "You can only take 1, 2 or 3 matches." << endl;
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-    else
-    {
-        matches -= input;
-        return matches;
-    }
+        cout << "There is " << + remainingMatches << " matches left!" << endl;
+        cout << "Player 1. Take 1, 2 or 3 Matches." << endl;
+        int input;
+        cin >> input;
 
+        if(!cin || input > 3 || input < 1)
+        {
+            cout << "You can only take 1, 2 or 3 matches." << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            
+        }
+        else
+        {
+            matches -= input;
+            return matches;
+        }
+         
+    }
+       
 }
-
 
 int main(int argc, char* argv[])
 {
